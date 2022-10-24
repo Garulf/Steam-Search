a0003f80ab8302fa25375038096058b26bf5ef2a
Garulf <535299+Garulf@users.noreply.github.com>
2022-10-24 04:53:37 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,10 @@
 import sys
 import os
 
-plugindir = os.path.abspath(os.path.dirname(__file__))
 plugin_root = sys.argv[0]
 os.chdir(os.path.dirname(os.path.abspath(plugin_root)))
-sys.path.append(plugindir)
-sys.path.append(os.path.join(plugindir, "lib"))
-sys.path.append(os.path.join(plugindir, "plugin"))
 
 from plugin.main import SteamSearch
 
 if __name__ == "__main__":
     SteamSearch()
-    
